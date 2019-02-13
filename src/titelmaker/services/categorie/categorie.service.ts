636287c769888947e6f8cc7dbd86f6848e3ff29a
Maarten <gregoiremaarten@gmail.com>
2019-02-13 17:32:09 +0100

    emotievierkant geeft nu 4 emotie's

--- src/titelmaker/services/categorie/categorie.service.ts
+++ src/titelmaker/services/categorie/categorie.service.ts
@@ -3,6 +3,8 @@ import {Observable, of} from 'rxjs';
 import {Arrays} from '../../util/arrays';
 import {categorieen} from '../../data/categorieen';
 import {Categorie} from '../../models/categorie';
+import {BijvoeglijkNaamwoord} from '../../models/bijvoeglijk-naamwoord';
+import {emoties} from '../../data/bijvoeglijk-naamwoorden/emoties';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,35 @@ export class CategorieService {
   constructor() { }
 
   bepaalCategorie(): Observable<string> {
-    return of(Arrays.bepaalWillekeurigElemntUitRij<Categorie>(categorieen).categorie);
+    const categorie: Categorie = Arrays.bepaalWillekeurigElemntUitRij<Categorie>(categorieen);
+    let categorieString: string = categorie.categorie;
+    if (categorieString === 'Emotievierkant') {
+      categorieString = this.voegEmotiesToe(categorieString);
+    }
+    return of(categorieString);
   }
 
-}
+  private voegEmotiesToe(categorie: string): string {
+      const emotiesVoorVierkant: BijvoeglijkNaamwoord[] = [Arrays.bepaalWillekeurigElemntUitRij<BijvoeglijkNaamwoord>(emoties)];
+
+      do {
+        emotiesVoorVierkant[1] = Arrays.bepaalWillekeurigElemntUitRij<BijvoeglijkNaamwoord>(emoties);
+      } while (emotiesVoorVierkant[1] === emotiesVoorVierkant[0]);
+
+      do {
+        emotiesVoorVierkant[2] = Arrays.bepaalWillekeurigElemntUitRij<BijvoeglijkNaamwoord>(emoties);
+      } while (emotiesVoorVierkant[2] === emotiesVoorVierkant[0] ||
+        emotiesVoorVierkant[2] === emotiesVoorVierkant[1]
+        );
+
+      do {
+        emotiesVoorVierkant[3] = Arrays.bepaalWillekeurigElemntUitRij<BijvoeglijkNaamwoord>(emoties);
+      } while (emotiesVoorVierkant[3] === emotiesVoorVierkant[0] ||
+      emotiesVoorVierkant[3] === emotiesVoorVierkant[1] ||
+      emotiesVoorVierkant[3] === emotiesVoorVierkant[2]);
+
+      categorie += ': ' + emotiesVoorVierkant[0].woord + ', ' + emotiesVoorVierkant[1].woord + ', ' +
+        emotiesVoorVierkant[2].woord + ', ' + emotiesVoorVierkant[3].woord;
+      return categorie;
+    }
+  }
